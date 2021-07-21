5b5b9e04aea7c53e3da9aa083be0c3a8c05e2bdc
Ryan Marsh <ryan@ryanmarsh.net>
2021-07-21 13:02:20 -0700

    Removed default pins (since they were incorrect anyway)

--- SimpleAnimaPowers.lua
+++ SimpleAnimaPowers.lua
@@ -7,44 +7,44 @@ local SAP = _G.LibStub("AceAddon-3.0"):NewAddon("SimpleAnimaPowers", "AceEvent-3
 _G.SAP = SAP
 
 local tableHasValue = function(t, v)
-    for key, val in pairs(t) do
-        if val == v then
-            return true
-        end
-    end
+	for key, val in pairs(t) do
+		if val == v then
+			return true
+		end
+	end
 
-    return false
+	return false
 end
 
-local torghastZones = {2162, 10472, 13400, 13403, 13404, 13411}
+local torghastZones = { 2162, 10472, 13400, 13403, 13404, 13411 }
 local inInstance, instanceType = _G.IsInInstance()
 
 local IsInTorghast = function()
-    local currentMapID = select(8, _G.GetInstanceInfo())
+	local currentMapID = select(8, _G.GetInstanceInfo())
 
-    return (inInstance and (tableHasValue(torghastZones, currentMapID)))
+	return (inInstance and (tableHasValue(torghastZones, currentMapID)))
 end
 
 local IsInMPlus = function()
-    local _, _, difficulty, _, _, _, _, _ = GetInstanceInfo()
-    local _, elapsed_time = GetWorldElapsedTime(1)
+	local _, _, difficulty, _, _, _, _, _ = GetInstanceInfo()
+	local _, elapsed_time = GetWorldElapsedTime(1)
 
-    return C_ChallengeMode.IsChallengeModeActive() and difficulty == 8 and elapsed_time >= 0
+	return C_ChallengeMode.IsChallengeModeActive() and difficulty == 8 and elapsed_time >= 0
 end
 
 local IsInSanctum = function()
-    return (inInstance and currentMapID == 13561)
+	return (inInstance and currentMapID == 13561)
 end
 
 SAP.GroupPowers = {}
 
 SAP.BackdropA = {
-    edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
-    bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
-    tile = true,
-    tileSize = 32,
-    edgeSize = 32,
-    insets = {left = 5, right = 5, top = 5, bottom = 5}
+	edgeFile = "Interface\\DialogFrame\\UI-DialogBox-Border",
+	bgFile = "Interface\\DialogFrame\\UI-DialogBox-Background",
+	tile = true,
+	tileSize = 32,
+	edgeSize = 32,
+	insets = { left = 5, right = 5, top = 5, bottom = 5 },
 }
 
 SAP.DataTree = {}
@@ -53,65 +53,63 @@ _G.SLASH_SAP1 = "/sap"
 _G.SLASH_SAP2 = "/simpleanimapowers"
 
 _G.SlashCmdList["SAP"] = function()
-    SAP:UpdateGroupPowers()
+	SAP:UpdateGroupPowers()
 
-    if not _G.SimpleAnimaPowersFrame:IsVisible() then
-        _G.SimpleAnimaPowersFrame:Show()
-    else
-        _G.SimpleAnimaPowersFrame:Hide()
-    end
+	if not _G.SimpleAnimaPowersFrame:IsVisible() then
+		_G.SimpleAnimaPowersFrame:Show()
+	else
+		_G.SimpleAnimaPowersFrame:Hide()
+	end
 end
 
 function SAP:OnInitialize()
-    SAP:RegisterEvent("ADDON_LOADED", "OnLoad")
+	SAP:RegisterEvent("ADDON_LOADED", "OnLoad")
 end
 
 function SAP:OnLoad(self, addon, ...)
-    if (addon == "SimpleAnimaPowers") then
-        SAP:RegisterEvent("CHAT_MSG_LOOT", "OnLoot")
+	if addon == "SimpleAnimaPowers" then
+		SAP:RegisterEvent("CHAT_MSG_LOOT", "OnLoot")
 
-        if not _G.SAPSettings then
-            _G.SAPSettings = SAP.DefaultConfig
-        end
+		if not _G.SAPSettings then
+			_G.SAPSettings = SAP.DefaultConfig
+		end
 
-        SAP.Settings = _G.SAPSettings
+		SAP.Settings = _G.SAPSettings
 
-        CR:RegisterOptionsTable("SimpleAnimaPowers", SAP.AceConfig, nil)
-        SAP.OptionsMenu = _G.LibStub("AceConfigDialog-3.0"):AddToBlizOptions("SimpleAnimaPowers", "SimpleAnimaPowers")
+		CR:RegisterOptionsTable("SimpleAnimaPowers", SAP.AceConfig, nil)
+		SAP.OptionsMenu = _G.LibStub("AceConfigDialog-3.0"):AddToBlizOptions("SimpleAnimaPowers", "SimpleAnimaPowers")
 
-        _G.SimpleAnimaPowersFrame = GUI:Create("Frame")
-        local frame = _G.SimpleAnimaPowersFrame
-        frame:SetWidth(400)
-        frame:SetHeight(300)
-        frame:SetPoint("LEFT", 50, 0, _G.UIParent)
-        frame:SetTitle("SimpleAnimaPowers")
-        frame:SetLayout("Fill")
-        frame:SetCallback(
-            "OnShow",
-            function()
-                SAP:OnShow(SAP)
-            end
-        )
+		_G.SimpleAnimaPowersFrame = GUI:Create("Frame")
+		local frame = _G.SimpleAnimaPowersFrame
+		frame:SetWidth(400)
+		frame:SetHeight(300)
+		frame:SetPoint("LEFT", 50, 0, _G.UIParent)
+		frame:SetTitle("SimpleAnimaPowers")
+		frame:SetLayout("Fill")
+		frame:SetCallback("OnShow", function()
+			SAP:OnShow(SAP)
+		end)
 
-        _G.SimpleAnimaPowersFrame_MainContainer = GUI:Create("ScrollFrame")
-        local container = _G.SimpleAnimaPowersFrame_MainContainer
-        container:SetLayout("List")
-        container:SetFullHeight(true)
-        container:SetFullWidth(true)
-        frame:AddChild(container)
+		_G.SimpleAnimaPowersFrame_MainContainer = GUI:Create("ScrollFrame")
+		local container = _G.SimpleAnimaPowersFrame_MainContainer
+		container:SetLayout("List")
+		container:SetFullHeight(true)
+		container:SetFullWidth(true)
+		frame:AddChild(container)
 
-        _G.tinsert(_G.UISpecialFrames, "SimpleAnimaPowersFrame")
-    end
+		_G.tinsert(_G.UISpecialFrames, "SimpleAnimaPowersFrame")
+	end
 end
 
-function SAP:OnLoot(self, event, ...)
-    if (event == "CHAT_MSG_LOOT") then
-        local message = select(1, ...)
+function SAP:OnLoot(event, ...)
+	print(...)
+	local message = select(1, ...)
 
-        if _G.strfind(message, MAW_POWER_DESCRIPTION) ~= nil then
-            if ((SAP.Settings.Tarragrue and IsInSanctum()) or (SAP.Settings.Torghast and IsInTorghast()) or IsInMPlus()) then
-                SAP:UpdateGroupPowers()
-            end
-        end
-    end
+	if _G.strfind(message, MAW_POWER_DESCRIPTION) ~= nil then
+		if (SAP.Settings.Tarragrue and IsInSanctum()) or (SAP.Settings.Torghast and IsInTorghast()) or IsInMPlus() then
+			print("fired")
+			SAP:UpdateGroupPowers()
+			SAP:UpdateGUI(SAP.GroupPowers)
+		end
+	end
 end
--- SimpleAnimaPowersFunctions.lua
+++ SimpleAnimaPowersFunctions.lua
@@ -8,208 +8,211 @@ local QTIP = LibStub("LibQTip-1.0")
 local DUMP = LibStub("LibTextDump-1.0")
 
 local tableHasValue = function(t, v)
-    for key, val in pairs(t) do
-        if val == tostring(v) then
-            return true
-        end
-    end
+	for key, val in pairs(t) do
+		if val == tostring(v) then
+			return true
+		end
+	end
 
-    return false
+	return false
 end
 
 local split = function(pString, pPattern)
-    local Table = {}
-    local fpat = "(.-)" .. pPattern
-    local last_end = 1
-    local s, e, cap = pString:find(fpat, 1)
+	local Table = {}
+	local fpat = "(.-)" .. pPattern
+	local last_end = 1
+	local s, e, cap = pString:find(fpat, 1)
 
-    while s do
-       if s ~= 1 or cap ~= "" then
-      table.insert(Table,cap)
-       end
-       last_end = e+1
-       s, e, cap = pString:find(fpat, last_end)
-    end
+	while s do
+		if s ~= 1 or cap ~= "" then
+			table.insert(Table, cap)
+		end
+		last_end = e + 1
+		s, e, cap = pString:find(fpat, last_end)
+	end
 
-    if last_end <= #pString then
-       cap = pString:sub(last_end)
-       table.insert(Table, cap)
-    end
-    return Table
+	if last_end <= #pString then
+		cap = pString:sub(last_end)
+		table.insert(Table, cap)
+	end
+	return Table
 end
 
-local pinnedPowers = function () 
-    return split(SAP.Settings.PinnedSpells, "\,")
+local pinnedPowers = function()
+	return split(SAP.Settings.PinnedSpells, ",")
 end
 
 function spairs(t, order)
-    local keys = {}
-    for k in pairs(t) do keys[#keys+1] = k end
+	local keys = {}
+	for k in pairs(t) do
+		keys[#keys + 1] = k
+	end
 
-    if order then
-        table.sort(keys, function(a,b) return order(t, a, b) end)
-    else
-        table.sort(keys)
-    end
+	if order then
+		table.sort(keys, function(a, b)
+			return order(t, a, b)
+		end)
+	else
+		table.sort(keys)
+	end
 
-    -- return the iterator function
-    local i = 0
-    return function()
-        i = i + 1
-        if keys[i] then
-            return keys[i], t[keys[i]]
-        end
-    end
+	-- return the iterator function
+	local i = 0
+	return function()
+		i = i + 1
+		if keys[i] then
+			return keys[i], t[keys[i]]
+		end
+	end
 end
 
 local GetGroupMembers = function(reversed, forceParty)
-    local unit = (not forceParty and _G.IsInRaid()) and "raid" or "party"
-    local numGroupMembers =
-        (forceParty and _G.GetNumSubgroupMembers() or _G.GetNumGroupMembers()) - (unit == "party" and 1 or 0)
-    local i = reversed and numGroupMembers or (unit == "party" and 0 or 1)
-    return function()
-        local ret
-        if i == 0 and unit == "party" then
-            ret = "player"
-        elseif i <= numGroupMembers and i > 0 then
-            ret = unit .. i
-        end
-        i = i + (reversed and -1 or 1)
-        return ret
-    end
+	local unit = (not forceParty and _G.IsInRaid()) and "raid" or "party"
+	local numGroupMembers = (forceParty and _G.GetNumSubgroupMembers() or _G.GetNumGroupMembers())
+		- (unit == "party" and 1 or 0)
+	local i = reversed and numGroupMembers or (unit == "party" and 0 or 1)
+	return function()
+		local ret
+		if i == 0 and unit == "party" then
+			ret = "player"
+		elseif i <= numGroupMembers and i > 0 then
+			ret = unit .. i
+		end
+		i = i + (reversed and -1 or 1)
+		return ret
+	end
 end
 
 local GetUnitNameWithColor = function(unit)
-    local _, class = _G.UnitClass(unit)
-    if not class then
-        return
-    end
-    return _G.RAID_CLASS_COLORS[class]:WrapTextInColorCode(UnitName(unit))
+	local _, class = _G.UnitClass(unit)
+	if not class then
+		return
+	end
+	return _G.RAID_CLASS_COLORS[class]:WrapTextInColorCode(UnitName(unit))
 end
 
 function SAP:OnShow(self)
-    if not _G.SimpleAnimaPowersFrame_MainContainer:IsShown() then
-        _G.SimpleAnimaPowersFrame_MainContainer:Show()
-    end
+	if not _G.SimpleAnimaPowersFrame_MainContainer:IsShown() then
+		_G.SimpleAnimaPowersFrame_MainContainer:Show()
+	end
 end
 
 function SAP:UpdateGUI(t)
-    local frame = _G.SimpleAnimaPowersFrame
-    local container = _G.SimpleAnimaPowersFrame_MainContainer
+	local frame = _G.SimpleAnimaPowersFrame
+	local container = _G.SimpleAnimaPowersFrame_MainContainer
 
-    container:ReleaseChildren()
+	container:ReleaseChildren()
 
-    -- loop through and add labels for each power + group member with power
-    local pinned = pinnedPowers()
-    local sortedKeys = {}
-    for k, v in pairs(t) do table.insert(sortedKeys, k) end
+	-- loop through and add labels for each power + group member with power
+	local pinned = pinnedPowers()
+	local sortedKeys = {}
+	for k, v in pairs(t) do
+		table.insert(sortedKeys, k)
+	end
 
-    table.sort(sortedKeys, function (a, b) 
-        local a_pinned = tableHasValue(pinned, a)
-        local b_pinned = tableHasValue(pinned, b)
+	table.sort(sortedKeys, function(a, b)
+		local a_pinned = tableHasValue(pinned, a)
+		local b_pinned = tableHasValue(pinned, b)
 
-        if (a_pinned and not b_pinned) then
-            return true
-        elseif (b_pinned and not a_pinned) then
-            return false
-        else
-            return a > b
-        end
-    end)
+		if a_pinned and not b_pinned then
+			return true
+		elseif b_pinned and not a_pinned then
+			return false
+		else
+			return tonumber(a) > tonumber(b)
+		end
+	end)
 
-    for _, k in ipairs(sortedKeys) do
-        local spellId = k
-        local spellLabel = GUI:Create("InteractiveLabel")
-        spellLabel:SetFullWidth(true)
-        spellLabel:SetFont(_G.GameFontNormalHuge2:GetFont())
-        spellLabel:SetCallback("OnEnter", function(widget)
-            local tooltip = GameTooltip;           
-            tooltip:SetOwner(UIParent, "ANCHOR_CURSOR");
-            tooltip:SetHyperlink(GetSpellLink(spellId))
-            tooltip:Show();
-        end)
-        spellLabel:SetCallback("OnLeave", function(widget) 
-            GameTooltip:FadeOut() 
-        end)
+	for _, k in ipairs(sortedKeys) do
+		local spellId = k
+		local spellLabel = GUI:Create("InteractiveLabel")
+		spellLabel:SetFullWidth(true)
+		spellLabel:SetFont(_G.GameFontNormalHuge2:GetFont())
+		spellLabel:SetCallback("OnEnter", function(widget)
+			local tooltip = GameTooltip
+			tooltip:SetOwner(UIParent, "ANCHOR_CURSOR")
+			tooltip:SetHyperlink(GetSpellLink(spellId))
+			tooltip:Show()
+		end)
+		spellLabel:SetCallback("OnLeave", function(widget)
+			GameTooltip:FadeOut()
+		end)
 
-        local _, _, icon = GetSpellInfo(spellId)
-        local ret = "|T" .. icon .. ":0|t " .. GetSpellLink(spellId) .. "  [" .. table.getn(t[k]) .. "]"
-        spellLabel:SetText(ret)
+		local _, _, icon = GetSpellInfo(spellId)
+		local ret = "|T" .. icon .. ":0|t " .. GetSpellLink(spellId) .. "  [" .. table.getn(t[k]) .. "]"
+		spellLabel:SetText(ret)
 
-        container:AddChild(spellLabel)
+		container:AddChild(spellLabel)
 
-        -- loop through members
+		-- loop through members
 
-        table.sort(t[k], function(a, b) return a:upper() < b:upper() end)
-        local unitsLabel = GUI:Create("Label")
-        unitsLabel:SetFullWidth(true)
-        unitsLabel:SetFont(_G.GameFontNormalLarge2:GetFont())
+		table.sort(t[k], function(a, b)
+			return a:upper() < b:upper()
+		end)
+		local unitsLabel = GUI:Create("Label")
+		unitsLabel:SetFullWidth(true)
+		unitsLabel:SetFont(_G.GameFontNormalLarge2:GetFont())
 
-        local allNames = "        "
-        for unitIndex, unitName in pairs(t[k]) do
+		local allNames = "        "
+		for unitIndex, unitName in pairs(t[k]) do
+			local name = GetUnitNameWithColor(unitName)
+			local last = (unitIndex == table.getn(t[k]))
 
-            local name = GetUnitNameWithColor(unitName)
-            local last = (unitIndex == table.getn(t[k]))
+			allNames = allNames .. name .. (last and "" or ", ")
+		end
 
-            allNames = allNames .. name .. (last and "" or ", ")
-        end
+		unitsLabel:SetText(allNames)
 
-        unitsLabel:SetText(allNames)
+		container:AddChild(unitsLabel)
 
-        container:AddChild(unitsLabel)
+		-- add spacer after every group (makes it look a bit nicer)
+		local spacer = GUI:Create("Label")
+		spacer:SetFullWidth(true)
+		spacer:SetHeight(10)
+		spacer:SetText(" ")
+		container:AddChild(spacer)
+	end
 
-        -- add spacer after every group (makes it look a bit nicer)
-        local spacer = GUI:Create("Label")
-        spacer:SetFullWidth(true)
-        spacer:SetHeight(10)
-        spacer:SetText(" ")
-        container:AddChild(spacer)
-    end
-
-    container:DoLayout()
+	container:DoLayout()
 end
 
 function SAP:UpdateGroupPowers()
-    -- reset powers list
-    SAP.GroupPowers = {}
-    SAP.DataTree = {}
+	-- reset powers list
+	SAP.GroupPowers = {}
+	SAP.DataTree = {}
 
-    local pinned = pinnedPowers()
+	local pinned = pinnedPowers()
 
-    -- pinned powers always show
-    if (table.getn(pinned) > 0) then
-        for _, spellId in pairs(pinned) do
-            SAP.GroupPowers[spellId] = {}
-        end
-    end
+	-- pinned powers always show
+	if table.getn(pinned) > 0 then
+		for _, spellId in pairs(pinned) do
+			SAP.GroupPowers[spellId] = {}
+		end
+	end
 
-    -- loop group members and populate table
-    for unit in GetGroupMembers() do
-        local unitName = _G.UnitName(unit)
-        local unitNameWithColor = GetUnitNameWithColor(unit)
-        local MAW_BUFF_MAX_DISPLAY = 44
-        for i = 1, MAW_BUFF_MAX_DISPLAY do
-            local spellName,
-                icon,
-                count,
-                debuffType,
-                duration,
-                expirationTime,
-                unitCaster,
-                isStealable,
-                shouldConsolidate,
-                spellId = _G.UnitAura(unit, i, "MAW")
-            if spellName ~= nil and spellId ~= nil then
-                local spellLink, _ = _G.GetSpellLink(spellId)
+	-- loop group members and populate table
+	for unit in GetGroupMembers() do
+		local unitName = _G.UnitName(unit)
+		local unitNameWithColor = GetUnitNameWithColor(unit)
+		local MAW_BUFF_MAX_DISPLAY = 44
+		for i = 1, MAW_BUFF_MAX_DISPLAY do
+			local spellName, icon, count, debuffType, duration, expirationTime, unitCaster, isStealable, shouldConsolidate, spellId =
+				_G.UnitAura(
+					unit,
+					i,
+					"MAW"
+				)
+			if spellName ~= nil and spellId ~= nil then
+				local spellLink, _ = _G.GetSpellLink(spellId)
 
-                if (SAP.GroupPowers[spellId] == nil) then
-                    SAP.GroupPowers[spellId] = {}
-                end
+				if SAP.GroupPowers[spellId] == nil then
+					SAP.GroupPowers[spellId] = {}
+				end
 
-                _G.tinsert(SAP.GroupPowers[spellId], unitName)
-            end
-        end
-    end
+				_G.tinsert(SAP.GroupPowers[spellId], unitName)
+			end
+		end
+	end
 
-    SAP:UpdateGUI(SAP.GroupPowers)
+	SAP:UpdateGUI(SAP.GroupPowers)
 end
