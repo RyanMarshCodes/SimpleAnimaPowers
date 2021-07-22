c7de9ee2b9d902ff4cf57d8622a081eabbdfc027
Ryan Marsh <ryan@ryanmarsh.net>
2021-07-22 12:53:27 -0700

    Updated lock frame strings for zhTW

--- Locale/zhTW.lua
+++ Locale/zhTW.lua
@@ -12,5 +12,5 @@ L["Enable for Mythic Dungeons"] = "在傳奇地城啟用"
 L["Pinned spell IDs"] = "釘選法術ID"
 L["Separate spell IDs by comma"] = "以逗號分隔法術ID"
 
-L["Lock Frame"] = true
-L["Lock frame position and size"] = true
+L["Lock Frame"] = "鎖定框架"
+L["Lock frame position and size"] = "鎖定框架的位置與大小"
